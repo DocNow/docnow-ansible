44a1043d551d60d33a9a9933a1556c2426111a85
Francis Kayiwa <kayiwa@pobox.com>
2017-09-03 12:58:17 -0400

    add ignore file for terraform

---
+++ docnow.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    access_key      = "AKIAJGAQJC5KV7GABSZQ"
+    secret_key      = "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"
+    region          = "us-east-1"
+}
+
+resource "aws_instance" "docnow" {
+    ami             = "ami-b374d5a5"
+    instance_type   = "t2.micro"
+}
+
+resource "aws_eip" "ip" {
+    instance        = "${aws_instance.docnow.id}"
+}
