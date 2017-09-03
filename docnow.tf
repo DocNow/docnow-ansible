e2ceaf431eb2d852ccc50607746160e47f80f310
Francis Kayiwa <kayiwa@pobox.com>
2017-09-03 13:08:24 -0400

    if we have more than one VM

--- docnow.tf
+++ docnow.tf
@@ -9,6 +9,11 @@ resource "aws_instance" "docnow" {
     instance_type   = "t2.micro"
 }
 
+resource "aws_instance" "docnow2" {
+    ami             = "ami-b374d5a5"
+    instance_type   = "t2.micro"
+}
+
 resource "aws_eip" "ip" {
     instance        = "${aws_instance.docnow.id}"
 }
